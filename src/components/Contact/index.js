767ad465c57740deab7a6cb5b45da22f8e232ccd
Mela Aliento <carmelaalientoZ@gmail.com>
2023-04-05 12:00:59 -0700

    Created folders

---
+++ src/components/Contact/index.js
@@ -0,0 +1,13 @@
+//  Impport from react
+import { useEffect } from 'react';
+
+
+const Contact = () => {
+    return (
+        <nav>
+            <h1>Contact</h1>
+        </nav>
+    );
+}
+
+export default Contact;
